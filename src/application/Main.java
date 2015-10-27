f520ed7555c675da7c86bb2b8a902b66b6ae8edb
Martin Virke <martin.virke@yh.nackademin.se>
2015-10-27 10:10:28 +0100

    Ett litet test

--- src/application/Main.java
+++ src/application/Main.java
@@ -14,8 +14,8 @@ public class Main extends Application {
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
-		//bleorp
+
+		//haskdh
 		
 	}
 	
