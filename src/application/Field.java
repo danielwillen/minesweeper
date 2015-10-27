8c844fe0b3a4ccfbb4a8d8f3cc06b73c0c48129c
OscarWahlberg <oscwa331@gmail.com>
2015-10-27 14:16:57 +0100

    Små ändringar av Field. Metoden NewBlankField skapad

--- src/application/Field.java
+++ src/application/Field.java
@@ -7,19 +7,18 @@ public class Field {
 	public int hight = 10;
 	public int width = 10;
 	int summa = 0;
-
-	public void fieldInit() {
-
-		Tile field[width][hight];
-
+	
+	Field field[][] = newBlankField(10,10);
+	
+	public Field newBlankField(int width, int height){
+		Tile field[][] = new Tile[width][height];
+		
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < hight; j++) {
 				field[i][j].setX(i);
 				field[i][j].setY(j);
 			}
 		}
+		return field;
 	}
-	
-	
-	
 }
