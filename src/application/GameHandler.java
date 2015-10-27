adf9f52f72e7705382a0d08c40f2b4b2bf6466f8
Daniel Willén <daniel.willen@gmail.com>
2015-10-27 13:40:37 +0100

    Added recursive cell method. Class needs to implement Tile Array.

--- src/application/GameHandler.java
+++ src/application/GameHandler.java
@@ -11,15 +11,10 @@ public class GameHandler {
 	GameHandler(){
 		
 		
-		if (!tileIsMined()) {
+		if (tileIsMined()==false) {
 	
-			if (hasNeighboors()){
-				exposeTile();
-			}
-			else // Expand cells with recursion, for now the functionality is the same.
-			{
-				exposeTile();
-			}
+			expose();
+		
 			
 		}
 		else { 
@@ -28,12 +23,64 @@ public class GameHandler {
 		
 	}
 	
-	
-	public void expandCells(){
-		
-		
+	private void expose(){
+		if (hasNeighboors()){
+			exposeSelf();
+		}
+		else // Expand cells with recursion, for now the functionality is the same.
+		{
+			exposeSurrounding();
+		}
 	}
 	
+	public void exposeSurrounding(){
+		final int surroundingCellCount=8;
+		
+		for (int i = 0; i < surroundingCellCount; i++) {
+			
+			//Coordinates for exposing neighbors (relative to the current cell):
+			int xOffset = 0;
+			int yOffset = 0; 
+			
+			switch (i) {
+			
+			case 0: {  xOffset=-1; yOffset=0;  }   break; //Left
+			case 1: {  xOffset=-1; yOffset=-1; }   break; //TopLeft
+			case 2: {  xOffset=0;  yOffset=-1; }   break; //Top
+			case 3: {  xOffset=1;  yOffset=-1; }   break; //TopRight
+			case 4: {  xOffset=1;  yOffset=0;  }   break; //Right
+			case 5: {  xOffset=1;  yOffset=1;  }   break; //BottomRight
+			case 6: {  xOffset=0;  yOffset=1;  }   break; //Bottom
+			case 7: {  xOffset=-1; yOffset=1;  }   break; //BottomLeft
+			
+			default: break;
+			}
+			
+			if (isNeighborWithinBoundary(xOffset,yOffset)){
+				expose();
+			}
+			
+	}
+		//For every surrounding cell that's not exposed:
+			//exposeTile(iterator)
+		
+		//Run this function on all neighbor cells that are not exposed and have a c
+		//Do this: Expose surrounding cells that are closed.
+	
+	
+	}
+	
+	private boolean isNeighborWithinBoundary(int xOffset, int yOffset){
+		//Needs field (cell array) to implement. 
+		//arrayWidth, arrayHeight, currentCellX, currentCellY;
+		//targetx = tile.getX() + xOffset
+		//targety = tile.getY() + yOffset
+		//if ((targetx > 0) && (targetx < arrayWidth)) 
+		// and if ((targety > 0) && (targety < arrayHeight)) 
+		//		return true
+		//return false
+		return true;
+	}
 	public boolean isGameOver() {
 		return isGameOver;
 	}
@@ -44,7 +91,7 @@ public class GameHandler {
 	}
 
 
-	private void exposeTile(){
+	private void exposeSelf(){
 		exposeCount++;
 		tile.setVisible(true);
 	}
