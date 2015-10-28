2c108a4b34c870181523587be737777a7f3f662b
OscarWahlberg <oscwa331@gmail.com>
2015-10-28 08:24:42 +0100

    mineLayer tar form

--- src/application/Field.java
+++ src/application/Field.java
@@ -4,13 +4,12 @@ public class Field {
 	public int height = 10;
 	public int width = 10;
 	int summa = 0;
-	
+
 	Tile[][] tileArray = newBlankField(height, width);
-	
-	
-	public Tile[][] newBlankField(int width, int height){
+
+	public Tile[][] newBlankField(int width, int height) {
 		Tile tileArray[][] = new Tile[width][height];
-		
+
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				tileArray[y][x] = new Tile(false, x, y);
@@ -18,21 +17,52 @@ public class Field {
 		}
 		return tileArray;
 	}
-	
-	public void printField(Tile tileArray[][]){
-		for (int y = 0; y < height; y++){
-			for (int x = 0; x < width; x++)	{
+
+	public void printField(Tile tileArray[][]) {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
 				System.out.print("X: " + tileArray[x][y].getX());
 				System.out.print(" Y: " + tileArray[x][y].getY());
 				System.out.println(" Mine: " + tileArray[x][y].isMine());
 			}
 		}
 	}
-	
-	public void mineLayer(){
-		
+
+	public void mineLayer(Tile tileArray[][], int mines) {
+		int minesLeft = mines;
+
+		// Kontrollera att antal mines som ska läggas får plats på spelplanen
+		// (height*width)
+		if (mines >= height * width) {
+			// Om inte, fyll skärmen med minor och meddela användaren att spelet
+			// kan bli lite för lätt att vinna.
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+					tileArray[x][y].setMine(true);
+					System.out.println("There will be mines... EVERYWHERE!");
+				}
+			}
+			minesLeft = 0;
+		}
+
+		// Annars
+		else {
+			// Så länge det finns minor kvar att lägga.
+			while (minesLeft > 0) {
+				// slumpa x och y-led med planens height och width som constraints
+				int currentX = 0; //Slumpa mellan 0 och width
+				int currentY = 0; //Slumpa mellan 0 och Height
+				
+				// Kontrollera om det redan ligger en mina där.
+				if (!tileArray[currentX][currentY].isMine())	{
+				// Om inte, lägg ut mina och räkna ner antal mines som finns
+				// kvar att lägga (minesLeft)
+					minesLeft--;
+				}
+			}
+		}
 	}
-	
+
 	public int getHeight() {
 		return height;
 	}
