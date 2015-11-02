53a6e8642ea5257c7411e816d6d59d4f91b6f790
holmblad91 <holmblad91@gmail.com>
2015-11-02 12:00:50 +0100

    Testtester

---
+++ src/MinesweeperTest/MainTest.java
@@ -0,0 +1,25 @@
+package MinesweeperTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+import application.Main;
+
+public class MainTest {
+
+	@Test(timeout=10)
+	public void testmain() {
+		Main testmain = new Main();
+		return;
+	}
+
+		
+		
+		
+	
+		
+	
+
+}
